8b77f23c910f6587dc8c6abff1e51760bd7714f1
Luis Castanho <lgcastanho16@gmail.com>
2024-03-15 14:26:54 -0300

    Mais melhorias no design

--- Main.py
+++ Main.py
@@ -1,10 +1,17 @@
 from tkinter import *
 
-def read_clicks():
+#Color Pallet
+amarelo_banana = '#F1EC5D'
+amarelo_highlight = '#FFF600'
+cinza = '#82929E'
+marrom = '#6B3E26'
+preto= '#000000'
+
+def read_clicks(): # Função para ler a pontuação salva em um arquivo.
     with open("data.txt", "r") as file:
         return int(file.read())
 
-def update_click(label,value):
+def update_click(label,value): #Função para atualizar a pontuação (no arquivo e na tela)
     cont = int(label.cget("text"))
     cont += value
     label.config(text=cont) 
@@ -12,54 +19,51 @@ def update_click(label,value):
         f.write(str(cont))  
 
 def MenuWindow(window,cont):
-    frame = Frame(window,background="#82929E")
+    frame = Frame(window,background=cinza)
     frame.pack(fill=BOTH, expand=True)
 
     BananaLabel = Label(frame,text='BANANA CLICKER',
                         font=('Impact',50,'underline'),
-                        bg='#82929E',fg='#F1EC5D')
+                        bg=cinza,fg=amarelo_banana)
     BananaLabel.place(anchor = 'n',relx=0.5,rely=0.1)
 
     StartButton = Button(frame, text='INICIAR',
                          font=('Impact',16),
-                         bg="#F1EC5D",fg='Black',
+                         bg=amarelo_banana,fg='Black',
                          command=lambda: MainWindow(window,cont),
-                         activebackground="#FFF600", 
-                         width=10, height=2, bd=3,relief=RAISED
+                         activebackground=amarelo_highlight, 
+                         width=10, height=2, bd=4,relief=RAISED
                          )
     StartButton.place(anchor='center', relx=0.5,rely=0.4)
 
-    OptionsButton = Button(frame, text='OPÇÕES',
+    MoreButton = Button(frame, text='MAIS',
                          font=('Impact',16),
-                         bg="#F1EC5D",fg='Black',
-                         command=lambda: MainWindow(window,cont),
-                         activebackground="#FFF600", 
-                         width=10, height=2, bd=3,relief=RAISED
+                         bg=amarelo_banana,fg='Black',
+                         command=lambda: MoreFunction(frame),
+                         activebackground=amarelo_highlight, 
+                         width=10, height=2, bd=4,relief=RAISED
                          )
-    OptionsButton.place(anchor='center', relx=0.5,rely=0.5)
+    MoreButton.place(anchor='center', relx=0.5,rely=0.5)
 
-    MoreButton = Button(frame, text='SOBRE',
-                         font=('Impact',16),
-                         bg="#F1EC5D",fg='Black',
-                         command=lambda: MainWindow(window,cont),
-                         activebackground="#FFF600", 
-                         width=10, height=2, bd=3,relief=RAISED
-                         )
-    MoreButton.place(anchor='center',relx=0.5,rely=0.6)
+def MoreFunction(frame):
+    label = Label(frame,text='Código e imagens criados por Luis Castanho\n',
+                  font=('Impact',16),fg=amarelo_banana,
+                  bg=cinza)
+    label.place(anchor='s',relx=0.5,rely=0.9)
 
 def MainWindow(window, cont):
     for widget in window.winfo_children():
         widget.destroy()
 
-    frame = Frame(window,background="#82929E")
+    frame = Frame(window,background=cinza)
     frame.pack(fill=BOTH, expand=True)
     
     imagem = PhotoImage(file='./images/Banana.png')
     button = Button(frame, 
                     image=imagem,
-                    bg="#82929E",
-                    activebackground="#82929E", 
-                    activeforeground="#82929E",
+                    bg=cinza,
+                    activebackground=cinza, 
+                    activeforeground=cinza,
                     bd=0,
                     command=lambda: update_click(label,1),
                     width=100,
@@ -69,104 +73,105 @@ def MainWindow(window, cont):
 
     label = Label(frame,
                   text=cont,
-                  font=('Arial',20,'bold'),
-                  background="#82929E",
-                  foreground="#f2f4f5")
+                  font=('Impact',35),
+                  background=cinza,
+                  foreground=amarelo_banana)
     label.place(relx=0.5, rely=0.1, anchor=N)
 
     StoreButton = Button(frame,
                          text='Loja',
                          font=('Impact',16),
                          foreground='Black',
-                         background='#F1EC5D',
-                         activebackground="#FFF600",  # Change active background color
+                         background=amarelo_banana,
+                         activebackground=amarelo_highlight,  
                          command=lambda: StoreWindow(window,cont),
-                         width=10,height=2,bd=3,relief=RAISED)
+                         width=10,height=2,bd=4,relief=RAISED)
     StoreButton.place(relx=0.5, rely=0.9, anchor='s')
 
 def StoreWindow(window,cont):
     for widget in window.winfo_children():
         widget.destroy()
 
-    frame = Frame(window, background="#82929E")
+    frame = Frame(window, background=cinza)
     frame.pack(fill=BOTH, expand=True)
 
     # Primeira Compra - Duas bananas
     # Label
     TwoBananas_Label = Label(frame,text="O que é melhor que uma banana? DUAS BANANAS!",
-                             bg='#6B3E26',fg='#F1EC5D',
-                             bd=0,relief=RIDGE,
-                             font=('Arial',16),
-                             width=50,height=2)
+                             bg=amarelo_banana,fg=preto,
+                             bd=2,relief=SOLID,
+                             font=('Impact',16),
+                             width=50,height=2
+                             )
     TwoBananas_Label.grid(row=0,column=0,pady=20,padx=50)
     # Imagem
     image1 = PhotoImage(file='./images/Banana2.png')
-    TwoBananas_Pic = Label(frame,image=image1,width=64,height=64,bg="#82929E")
+    TwoBananas_Pic = Label(frame,image=image1,width=64,height=64,bg=cinza)
     TwoBananas_Pic.image = image1
     TwoBananas_Pic.grid(row=0,column=1,pady=20,padx=20)
     # Botao
     TwoBananas_Button = Button(frame,text='Comprar',
                                font=('Impact',16),
-                               foreground='Black',
-                               background='#F1EC5D',
-                               activebackground="#FFF600",  # Change active background color
-                               width=10, height=2,bd=3,relief=RAISED)
+                               foreground=preto,
+                               background=amarelo_banana,
+                               activebackground=amarelo_highlight,  
+                               width=10, height=2,bd=4,relief=RAISED)
     TwoBananas_Button.grid(row=0,column=2,pady=20,padx=20)
 
     # Segunda Compra - Três Bananas
     # Label
     ThreeBananas = Label(frame,text="TRÊS?????",
-                         bg='#6B3E26',fg='#F1EC5D',
-                         bd=0,relief=RIDGE,
-                         font=('Arial',16),
+                         bg=amarelo_banana,fg=preto,
+                         bd=2,relief=SOLID,
+                         font=('Impact',16),
                          width=50,height=2)
     ThreeBananas.grid(row=1,column=0,pady=20)
 
     # Imagem
     image2 = PhotoImage(file='./images/Banana3.png')
-    ThreeBananas_Pic = Label(frame,image=image2,width=64,height=64,bg="#82929E")
+    ThreeBananas_Pic = Label(frame,image=image2,width=64,height=64,bg=cinza)
     ThreeBananas_Pic.image = image2
     ThreeBananas_Pic.grid(row=1,column=1,pady=20,padx=20)
     # Botao
     ThreeBananas_Button = Button(frame,text='Comprar',
                                  font=('Impact',16),
-                                 foreground='Black',
-                                 background='#F1EC5D',
-                                 activebackground="#FFF600",  # Change active background color
-                                 width=10, height=2,bd=3,relief=RAISED)
+                                 foreground=preto,
+                                 background=amarelo_banana,
+                                 activebackground=amarelo_highlight, 
+                                 width=10, height=2,bd=4,relief=RAISED)
     ThreeBananas_Button.grid(row=1,column=2,pady=20,padx=50)
 
     # Terceira Compra - Cacho de Banana
     # Label
     BananaBunch = Label(frame,text="Compra um cacho todo...",
-                         bg='#6B3E26',fg='#F1EC5D',
-                         bd=0,relief=RIDGE,
-                         font=('Arial',16),
+                         bg=amarelo_banana,fg=preto,
+                         bd=2,relief=SOLID,
+                         font=('Impact',16),
                          width=50,height=2)
     BananaBunch.grid(row=2,column=0,pady=20)
 
     # Imagem
     image2 = PhotoImage(file='./images/BananaCacho.png')
-    BananaBunch_Pic = Label(frame,image=image2,width=64,height=64,bg="#82929E")
+    BananaBunch_Pic = Label(frame,image=image2,width=64,height=64,bg=cinza)
     BananaBunch_Pic.image = image2
     BananaBunch_Pic.grid(row=2,column=1,pady=20,padx=20)
     # Botao
     BananaBunch = Button(frame,text='Comprar',
                                  font=('Impact',16),
-                                 foreground='Black',
-                                 background='#F1EC5D',
-                                 activebackground="#FFF600",  # Change active background color
-                                 width=10, height=2,bd=3,relief=RAISED)
+                                 foreground=preto,
+                                 background=amarelo_banana,
+                                 activebackground=amarelo_highlight, 
+                                 width=10, height=2,bd=4,relief=RAISED)
     BananaBunch.grid(row=2,column=2,pady=20,padx=50)
 
     # Botao Retornar
     Return_Button = Button(frame,text='Voltar',
                            command=lambda: MainWindow(window, cont),
                            font=('Impact',16),
-                           foreground='Black',
-                           background='#F1EC5D',
-                           activebackground="#FFF600",  # Change active background color
-                           width=10, height=2,bd=3,relief=RAISED)
+                           foreground=preto,
+                           background=amarelo_banana,
+                           activebackground=amarelo_highlight,  
+                           width=10, height=2,bd=4,relief=RAISED)
     Return_Button.grid(row=4,column=2,pady=20,padx=50)
 
 def main():
